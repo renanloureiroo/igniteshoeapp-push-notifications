f090641c29e40012c0a5006a3f18f274ad48fd21
Renan Pereira Loureiro <renan.pereiral@sonda.com>
2023-11-07 13:15:13 -0300

    feat: setup push notifications in android

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from './src/routes';
+import { LogLevel, OneSignal } from "react-native-onesignal";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { Routes } from "./src/routes";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
+
+import { CartContextProvider } from "./src/contexts/CartContext";
+
+OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+OneSignal.initialize("9d1c8713-724f-484a-84d6-23eb92dc382b");
+
+// Also need enable notifications to complete OneSignal setup
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
